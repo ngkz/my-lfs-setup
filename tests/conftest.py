5ac641503b67aee05569e0db320603b1718a64e3
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2020-01-14 13:45:34 +0900

    add /_ext to sys.path when conftest.py is being loaded

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,11 @@ import pytest
 from sphinx.testing.path import path
 import sys
 
+sys.path.insert(0, path(__file__).parent.parent.abspath() / '_ext')
+
 pytest_plugins = 'sphinx.testing.fixtures'
 
 # Code by the Sphinx team
 @pytest.fixture(scope='session')
 def rootdir():
     return path(__file__).parent.abspath() / 'roots'
-
-@pytest.fixture(scope='session', autouse=True)
-def insert_af2lfs_ext_path():
-    sys.path.insert(0, path(__file__).parent.parent.abspath() / '_ext')
-    yield
-    sys.path.pop(0)
