0b7f43fd19e3e48392c2ce250650e775a4878964
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2020-04-18 01:50:07 +0900

    unquote_fssafe()

--- _ext/af2lfs/utils.py
+++ _ext/af2lfs/utils.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
+import os
 import shutil
 
 def get_load():
@@ -8,3 +9,37 @@ def get_load():
 
     with open('/proc/loadavg') as loadavg:
         return int(loadavg.read().split()[3].split('/')[0]) - 1
+
+FSUNSAFE_WINDOWS = b'\0"*/:<>?\\|'
+FSUNSAFE_POSIX = b'\0/'
+
+def unquote_fssafe(string, encoding='utf-8', errors='replace'):
+    chrs = string.encode('utf-8')
+    result = []
+
+    if os.name == 'nt':
+        fsunsafe = FSUNSAFE_WINDOWS
+    elif os.name == 'posix':
+        fsunsafe = FSUNSAFE_POSIX
+    else:
+        raise NotImplementedError
+
+    i = 0
+    while i < len(chrs):
+        ch = chrs[i]
+
+        if ch == ord('%') and len(chrs) - i >= 3:
+            try:
+                ch = int(chrs[i+1:i+1+2], 16)
+                i += 2
+            except ValueError:
+                pass
+
+        if ch in fsunsafe:
+            result.extend(b'%%%02x' % ch)
+        else:
+            result.append(ch)
+
+        i += 1
+
+    return bytes(result).decode(encoding, errors)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,23 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import pytest
 from unittest import mock
-from af2lfs.utils import get_load
+from af2lfs.utils import get_load, unquote_fssafe
 
 def test_get_load():
     m = mock.mock_open(read_data='0.47 0.49 0.59 3/1130 16077')
     with mock.patch('af2lfs.utils.open', m):
         assert get_load() == 2
+
+@mock.patch('os.name', 'posix')
+def test_unquote_fssafe_posix():
+    assert unquote_fssafe('A%42%1Z%00%2f/\0') == 'AB%1Z%00%2f%2f%00'
+
+@mock.patch('os.name', 'nt')
+def test_unquote_fssafe_windows():
+    assert unquote_fssafe('A%42%1Z%00%22%2a%2f%3a%3c%3e%3f%5c%7c\0"*/:<>?\\|') == \
+        'AB%1Z%00%22%2a%2f%3a%3c%3e%3f%5c%7c%00%22%2a%2f%3a%3c%3e%3f%5c%7c'
+
+@mock.patch('os.name', 'foobar')
+def test_unquote_fssafe_unknown_os():
+    with pytest.raises(NotImplementedError):
+        unquote_fssafe('a')
